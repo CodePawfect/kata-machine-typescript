1ff4444e6516d9f881e063eadffd2da0959b98de
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-08 17:45:24 +0200

    add Depth-First-Search implementation on a Binary Search Tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function find(node: BinaryNode<number> | null, needle: number): boolean {
+    if(!node) {
+        return false;
+    }
 
+    if(node.value === needle) {
+        return true;
+    }
+
+    if(needle > node.value) {
+        return find(node.right, needle);
+    } else {
+        return find(node.left, needle);
+    }
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return find(head, needle);
 }
