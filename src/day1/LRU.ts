444508d2d219df22d9715626eed37787cda22778
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-14 18:22:17 +0200

    implement least recently used datastructure.

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,110 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+}
+
+function createNode<V>(value: V): Node<V> {
+    return {value};
+}
+
+/**
+ *Represents a data structure based on the least recently used (LRU) principle,
+  commonly seen in caches. It has a time complexity of O(1) for get and update operations.
+ */
 export default class LRU<K, V> {
     private length: number;
+    private node: Node<V>;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map();
+        this.reverseLookup = new Map();
     }
 
     update(key: K, value: V): void {
+        let node = this.lookup.get(key);
+        if(!node) {
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimCache();
 
-}
+            this.lookup.set(key, node);
+            this.reverseLookup.set(node, key);
+        } else {
+            this.detach(node);
+            this.prepend(node);
+            node.value = value;
+        }
+
+    }
+
     get(key: K): V | undefined {
+        const node = this.lookup.get(key);
+        if(!node) {
+            return undefined;
+        }
 
-}
+        this.detach(node);
+        this.prepend(node);
+
+        return node.value;
+    }
+
+    private detach(node: Node<V>): void {
+        if(node.prev) {
+            node.prev.next = node.next;
+        }
+
+        if(node.next) {
+            node.next.prev = node.prev;
+        }
+
+        if(this.length === 1) {
+            this.tail = this.head = undefined;
+        }
+
+        if(this.head === node) {
+            this.head = this.head.next
+        }
+
+        if(this.tail === node) {
+            this.tail = this.tail.prev
+        }
+
+        node.next = undefined;
+        node.prev = undefined;
+    }
+
+    private prepend(node: Node<V>): void {
+        if(!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+
+    private trimCache(): void {
+        if(this.length <= this.capacity) {
+            return;
+        }
+
+        const tail = this.tail as Node<V>;
+        this.detach(this.tail as Node<V>);
+
+        const key = this.reverseLookup.get(tail) as K;
+        this.lookup.delete(key);
+        this.reverseLookup.delete(tail);
+        this.length--;
+    }
 }
